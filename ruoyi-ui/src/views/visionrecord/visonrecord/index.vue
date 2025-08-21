8f46e03e54f3919aecbd17ec2fdad709d1cbbb2a
DYH414 <737760221@qq.com>
2025-08-21 10:53:44 +0800

    更新视力记录模块：将诊断输入框更改为下拉选择框，移除创建时间和更新时间字段，优化了表单结构。

--- ruoyi-ui/src/views/visionrecord/visonrecord/index.vue
+++ ruoyi-ui/src/views/visionrecord/visonrecord/index.vue
@@ -28,28 +28,14 @@
         />
       </el-form-item>
       <el-form-item label="诊断" prop="diagnosis">
-        <el-input
-          v-model="queryParams.diagnosis"
-          placeholder="请输入诊断"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="创建时间" prop="createTime">
-        <el-date-picker clearable
-          v-model="queryParams.createTime"
-          type="date"
-          value-format="yyyy-MM-dd"
-          placeholder="请选择创建时间">
-        </el-date-picker>
-      </el-form-item>
-      <el-form-item label="更新时间" prop="updateTime">
-        <el-date-picker clearable
-          v-model="queryParams.updateTime"
-          type="date"
-          value-format="yyyy-MM-dd"
-          placeholder="请选择更新时间">
-        </el-date-picker>
+        <el-select v-model="queryParams.diagnosis" placeholder="请选择诊断" clearable>
+          <el-option
+            v-for="dict in dict.type.VISION_LEVEL"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          ></el-option>
+        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
@@ -121,16 +107,6 @@
         </template>
       </el-table-column>
       <el-table-column label="备注" align="center" prop="remark" />
-      <el-table-column label="创建时间" align="center" prop="createTime" width="180">
-        <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.createTime, '{y}-{m}-{d}') }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="更新时间" align="center" prop="updateTime" width="180">
-        <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.updateTime, '{y}-{m}-{d}') }}</span>
-        </template>
-      </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="scope">
           <el-button
@@ -247,9 +223,7 @@ export default {
         personId: null,
         checkDate: null,
         year: null,
-        diagnosis: null,
-        createTime: null,
-        updateTime: null
+        diagnosis: null
       },
       // 表单参数
       form: {},
